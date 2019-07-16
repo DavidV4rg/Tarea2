16b5225076fd719681225d8c4655255d810a5530
Default user <id.vargas@uniandes.edu.co>
2019-07-16 17:59:49 +0000

    Se hace la hibrida

--- Fourier.py
+++ Fourier.py
@@ -23,20 +23,22 @@ plt.colorbar()
 plt.savefig("FFT2D2.png")
 
 #Se crean los filtros de la imagen
-filtro1 = 90
+filtro1 = 100
 index1 = np.where(trans1 > filtro1)
 trans1[index1] = 0
 
 plt.figure()
 plt.imshow(np.abs(trans1), norm = LogNorm())
+plt.colorbar()
 plt.savefig("flitro_imagen_1")
 
-filtro2 = 90
-index2 = np.where(trans2 > filtro2)
+filtro2 = 0
+index2 = np.where(trans2 < filtro2)
 trans2[index2] = 0
 
 plt.figure()
 plt.imshow(np.abs(trans2), norm = LogNorm())
+plt.colorbar()
 plt.savefig("filtro_imagen_2")
 
 filt1 = ifft2(trans1)
@@ -49,9 +51,10 @@ plt.figure()
 plt.imshow(np.real(filt2), plt.cm.gray)
 plt.savefig("im2_filtrada")
 
+suma = np.real(filt2)+np.real(filt1)
+
 plt.figure()
-plt.imshow(np.real(filt2), plt.cm.gray)
-plt.imshow(np.real(filt1), plt.cm.gray)
+plt.imshow(suma, plt.cm.gray)
 plt.savefig("hibrida")
 
 
