676916dcfe3ccd958e7f82670c301c0de59af727
Default user <id.vargas@uniandes.edu.co>
2019-07-17 23:15:02 +0000

    deltas adecuados

--- ODEs.cpp
+++ ODEs.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-//float años = pow(31.54,7);
+double masaT = 5.972*pow(10,24);
 
 double dxdt_x(double ti, double xi, double vx);
 double dxdt_y(double ti, double yi, double vy);
@@ -58,7 +58,7 @@ double euler(double t0, double tf, double dxy, int puntos, string name)
     ofstream outfile;
     outfile.open(name);
     double dt = (tf-t0)/puntos;
-    double t[puntos], x[puntos], y[puntos], vx[puntos], vy[puntos], r12[puntos];
+    double t[puntos], x[puntos], y[puntos], vx[puntos], vy[puntos], r12[puntos], ma[puntos], energiaP[puntos], energiaK[puntos];
     t[0] = 0;
     x[0] = 0.1163;
     y[0] = 0.9772;
@@ -77,11 +77,12 @@ double euler(double t0, double tf, double dxy, int puntos, string name)
         r12[i] = sqrt(pow(x[i],2)+pow(y[i],2));
         vx[i] = vx[i-1] + (dxy*dvdt_x(t[i-1], x[i-1], r12[i-1]));
         vy[i] = vy[i-1] + (dxy*dvdt_y(t[i-1], y[i-1], r12[i-1]));
+        ma[i] = (r12[i]*vx[i])-(r12[i]*vy[i]);
     }
     
     for (int i = 1; i<puntos; i++)
     {
-        outfile << t[i-1] << "||" << x[i-1] << "||" << y[i-1] << "||" << vx[i-1] << "||" << vy[i-1] << endl;
+        outfile << t[i-1] << "||" << x[i-1] << "||" << y[i-1] << "||" << vx[i-1] << "||" << vy[i-1] << "||" << ma[i] <<endl;
     }
     outfile.close();
 }
@@ -93,7 +94,7 @@ double leapfrog(double t0, double tf, double dxy, int puntos, string name1)
     ofstream outfile1;
     outfile1.open(name1);
     double dt = (tf-t0)/puntos;
-    double t[puntos], x[puntos], y[puntos], vx[puntos], vy[puntos], r12[puntos];
+    double t[puntos], x[puntos], y[puntos], vx[puntos], vy[puntos], r12[puntos], ma[puntos], energiaP[puntos], energiaK[puntos];
     t[0] = 0;
     x[0] = 0.1163;
     y[0] = 0.9772;
@@ -111,11 +112,12 @@ double leapfrog(double t0, double tf, double dxy, int puntos, string name1)
         y[i] = y[i-1] + (0.5*dxy*dxdt_y(t[i-1],y[i-1],vy[i-1]));
         r12[i] = sqrt(pow(x[i],2)+pow(y[i],2));
         vx[i] = vx[i-1] + (dxy*dvdt_x(t[i-1],x[i-1],r12[i-1]));
-        vy[i] = vy[i-1] + (dxy*dvdt_y(t[i-1],y[i-1],r12[i-1]));        
+        vy[i] = vy[i-1] + (dxy*dvdt_y(t[i-1],y[i-1],r12[i-1]));   
+        ma[i] = (r12[i]*vx[i])-(r12[i]*vy[i]);
     }    
     for (int i =1; i<puntos; i++)
     {
-        outfile1 << t[i-1] << "||" << x[i-1] << "||" << y[i-1] << "||" << vx[i-1] << "||" << vy[i-1] << endl;
+        outfile1 << t[i-1] << "||" << x[i-1] << "||" << y[i-1] << "||" << vx[i-1] << "||" << vy[i-1] << "||" << ma[i] <<endl;
         
     }
     outfile1.close();
@@ -128,7 +130,7 @@ double rungekutta(double t0, double tf, double dxy, int puntos, string name2)
 {
     ofstream outfile2;
     outfile2.open(name2);
-    double t[puntos], x[puntos], y[puntos], vx[puntos], vy[puntos], r12[puntos];
+    double t[puntos], x[puntos], y[puntos], vx[puntos], vy[puntos], r12[puntos], ma[puntos], energiaP[puntos], energiaK[puntos];
     //Condiciones iniciales
     t[0] = 0;
     x[0] = 0.1163;
@@ -172,20 +174,21 @@ double rungekutta(double t0, double tf, double dxy, int puntos, string name2)
         k4v_x = dxy*dvdt_x(t[i-1]+dxy, x[i-1]+k3x, r12[i-1]+k3v_x);
         k4v_y = dxy*dvdt_y(t[i-1]+dxy, y[i-1]+k3y, r12[i-1]+k3v_y);   
       
-        meanx = (1/6)*(k1x+(2*k2x)+(2*k3x)+k4x);
-        meany = (1/6)*(k1y+(2*k2y)+(2*k3y)+k4y);
-        meanv_x = (1/6)*(k1v_x+(2*k2v_x)+(2*k3v_x)+k4v_x);
-        meanv_y = (1/6)*(k1v_y+(2*k2v_y)+(2*k3v_y)+k4v_y);
+        meanx = (1.0/6.0)*(k1x+(2*k2x)+(2*k3x)+k4x);
+        meany = (1.0/6.0)*(k1y+(2*k2y)+(2*k3y)+k4y);
+        meanv_x = (1.0/6.0)*(k1v_x+(2*k2v_x)+(2*k3v_x)+k4v_x);
+        meanv_y = (1.0/6.0)*(k1v_y+(2*k2v_y)+(2*k3v_y)+k4v_y);
         
         x[i] = x[i-1]+meanx;
         y[i] = y[i-1]+meany;
         r12[i] = sqrt(pow(x[i],2)+pow(y[i],2));
         vx[i] = vx[i-1]+meanv_x;
         vy[i] = vy[i-1]+meanv_y;
+        ma[i] = (r12[i]*vx[i])-(r12[i]*vy[i]);
     }
     for(int i =1; i<puntos; i++)
     {
-        outfile2 << t[i-1] << "||" << x[i-1] << "||" << y[i-1] << "||" << vx[i-1] << "||" << vy[i-1] << endl;        
+        outfile2 << t[i-1] << "||" << x[i-1] << "||" << y[i-1] << "||" << vx[i-1] << "||" << vy[i-1] << "||" << ma[i] <<endl;
     }
     
     outfile2.close();
@@ -194,19 +197,19 @@ double rungekutta(double t0, double tf, double dxy, int puntos, string name2)
 int main()
 {
     //Primer dt
-    euler(0, 20, 0.001, 10050, "Euler1.dat");
-    leapfrog(0, 20, 0.01, 10050, "LF1.dat");
-    rungekutta(0, 20, 0.001, 10050, "RK1.dat");
+    euler(0, 20, 0.001, 50000, "Euler1.dat");
+    leapfrog(0, 20, 0.001, 20000, "LF1.dat");
+    rungekutta(0, 20, 0.0001, 40000, "RK1.dat");
     
     //Segundo dt
-    euler(0, 20, 0.001, 10000, "Euler2.dat");
-    leapfrog(0, 20, 0.001, 10000, "LF2.dat");
-    rungekutta(0, 20, 0.001, 10000, "RK2.dat");
+    euler(0, 20, 0.0001, 50000, "Euler2.dat");
+    leapfrog(0, 20, 0.0001, 20000, "LF2.dat");
+    rungekutta(0, 20, 0.0001, 40000, "RK2.dat");
     
     //Tercer dt
-    euler(0, 20, 0.001, 20000, "Euler3.dat");
-    leapfrog(0, 20, 0.0001, 20000, "LF3.dat");
-    rungekutta(0, 20, 0.001, 20000, "RK3.dat");
+    euler(0, 20, 0.0002, 52000, "Euler3.dat");
+    leapfrog(0, 20, 0.0002, 22000, "LF3.dat");
+    rungekutta(0, 20, 0.0002, 42000, "RK3.dat");
     return 0;
 }
 
