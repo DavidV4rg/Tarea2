701a829deaba6d1ae4f376434c4e148148b7e2b5
Default user <id.vargas@uniandes.edu.co>
2019-07-19 03:48:23 +0000

    Graficas finales

--- Fourier.py
+++ Fourier.py
@@ -16,27 +16,16 @@ plt.figure(figsize=(10,8))
 plt.subplot(1,2,1)
 plt.imshow(np.abs(trans1), norm = LogNorm() )
 plt.colorbar()
-plt.title("Espectro de Fourier para imagen feliz")
+plt.title("$Espectro$ $de$ $Fourier$ $imagen$ $feliz$")
 plt.subplot(1,2,2)
 plt.imshow(np.abs(trans2), norm = LogNorm() )
-plt.title("Espectro de Fourier para imagen seria")
+plt.title("$Espectro$ $de$ $Fourier$ $imagen$ $seria$")
 plt.colorbar()
-plt.savefig("FFT2D1-2.pdf")
+plt.savefig("FFtIm.pdf")
 
 #Se calculan las frecuencias de cada imagen
 fr1 = fftshift(trans1)
 fr2 = fftshift(trans2)
-
-plt.figure(figsize=(10,8))
-plt.subplot(1,2,1)
-plt.imshow(np.abs(fr1), norm=LogNorm())
-plt.title("Frecuencias imagen feliz")
-plt.colorbar()
-plt.subplot(1,2,2)
-plt.imshow(np.abs(fr2), norm=LogNorm())
-plt.title("Frecuencias imagen feliz")
-plt.colorbar()
-plt.savefig("frecuencias.pdf")
 
 #Se crean los filtros de la imagen
 filtro1 = 0
@@ -46,32 +35,41 @@ trans1[index1] = 0
 filtro2 = 45
 index2 = np.where(trans2 > filtro2)
 trans2[index2] = 0
-
-plt.figure(figsize=(11,8))
-plt.subplot(1,2,1)
-plt.imshow(np.abs(trans1), norm = LogNorm())
-plt.colorbar()
-plt.title("flitro imagen 1")
-plt.subplot(1,2,2)
-plt.imshow(np.abs(trans2), norm = LogNorm())
-plt.colorbar()
-plt.title("filtro imagen 2")
-plt.savefig("filtros.pdf")
-
 filt1 = ifft2(trans1)
 filt2 = ifft2(trans2)
 suma = np.real(filt2)+np.real(filt1)
 
-plt.figure(figsize=(12,8))
-plt.subplot(1,3,1)
+plt.figure(figsize=(20,30))
+plt.subplot(3,2,1)
+plt.imshow(np.abs(fr1), norm=LogNorm())
+plt.title("Frecuencias imagen feliz")
+plt.colorbar()
+plt.subplot(3,2,2)
+plt.imshow(np.abs(fr2), norm=LogNorm())
+plt.title("Frecuencias imagen feliz")
+plt.colorbar()
+
+plt.subplot(3,2,3)
+plt.imshow(np.abs(trans1), norm = LogNorm())
+plt.colorbar()
+plt.title("flitro imagen 1")
+plt.subplot(3,2,4)
+plt.imshow(np.abs(trans2), norm = LogNorm())
+plt.colorbar()
+plt.title("filtro imagen 2")
+
+plt.subplot(3,2,5)
 plt.imshow(np.real(filt1), plt.cm.gray)
 plt.title("im1_flitrada")
-plt.subplot(1,3,2)
+plt.subplot(3,2,6)
 plt.imshow(np.real(filt2), plt.cm.gray)
 plt.title("im2_filtrada")
-plt.subplot(1,3,3)
+plt.savefig("ImProceso.pdf")
+
+
+plt.figure()
 plt.imshow(suma, plt.cm.gray)
 plt.title("hibrida")
-plt.savefig("filtradas.pdf")
+plt.savefig("ImHybrid.pdf")
 
 
